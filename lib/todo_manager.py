5080bd4ff5c2e84e4d6ba779a8371f894178f082
Samantha Goldstein <samantha.goldstein31@gmail.com>
2020-04-18 13:59:13 -0400

    moving todo manager into own file

---
+++ lib/todo_manager.py
@@ -0,0 +1,95 @@
+import requests
+import json
+from datetime import date
+
+from . import secrets
+from . import sample
+
+PREFIX_STRING = """
+☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ
+    Good morning goldsam!
+☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ
+
+Here are today's tasks:
+"""
+
+class TodoManager:
+
+    def getToDoListString(self): 
+        # Not totally sure which technique i need for this
+        # so going to leave this here for posterity if i need this
+        #useSyncLibrary()
+        return self.getInfoFromRestAPI()
+
+    def getInfoFromRestAPI(self):
+        today_and_overdue = self.getRequestData()
+        (today_tasks, overdue_tasks) = self.getTodayAndOverdue(today_and_overdue)
+        return self.getTasksString(today_tasks, overdue_tasks)
+
+
+    def getRequestData(self):
+        return requests.get(
+            "https://api.todoist.com/rest/v1/tasks",
+            params={
+                "filter":  self.getFilterString()
+            },
+            headers={
+                "Authorization": self.getHeaderString()
+            }
+        ).json()
+
+
+    def getFilterString(self):
+        today_date = date.today().isoformat()
+        return "(due: %s | overdue)" % today_date
+
+    def getHeaderString(self):
+        return "Bearer %s" % secrets.API_TOKEN
+
+
+    def getTasksString(self, today, overdue):
+
+        s = PREFIX_STRING
+        for task in today:
+            s += "□ %s\n" % task['content']
+        s += "\nHere are your overdue tasks:\n"
+        for task in overdue:
+            s += "□ %s\n" % task['content']
+        
+        s += "\n\n\n"
+
+        return s
+
+
+    def getTodayAndOverdue(self, all_tasks):
+        today = []
+        overdue = []
+        today_date = date.today().isoformat()
+
+        for task in all_tasks:
+            due_date = task['due']['date']
+            if (due_date == today_date):
+                today.append(task)
+            else:
+                overdue.append(task)
+
+        return (today, overdue)
+
+    def useSyncLibrary(self):
+        api = TodoistAPI(secrets.API_TOKEN)
+        api.sync()
+        #print(api.state['projects'])
+        print("\n\n\n")
+        items = api.state["items"]
+        print(json.dumps(items))
+
+        # get all completed
+        all_comp = api.completed.get_all()
+
+        for item in items:
+            if item['checked'] > 0:
+                print(item['content'])
+
+
+if __name__ == '__main__':
+    print("todo_manager.py")
--- todoistmain.py
+++ todoistmain.py
@@ -2,7 +2,7 @@ import requests
 import json
 from lib import secrets
 from lib import sample
-from lib.todos import Todos
+from lib.todo_manager import TodoManager
 from datetime import date
 
 # for Sync api
@@ -12,91 +12,11 @@ from todoist.api import TodoistAPI
 # leaving here for now in case i need this...
 
 def main():
-    todo_manager = Todos()
+    todo_manager = TodoManager()
     todo_string = todo_manager.getToDoListString()
     print(todo_string)
 
 
-def getToDoListString():
-    # Not totally sure which technique i need for this
-    # so going to leave this here for posterity if i need this
-    #useSyncLibrary()
-    return getInfoFromRestAPI()
-
-def getInfoFromRestAPI():
-    today_and_overdue = getRequestData()
-    (today_tasks, overdue_tasks) = getTodayAndOverdue(today_and_overdue)
-    return getTasksString(today_tasks, overdue_tasks)
-
-
-def getRequestData():
-    return requests.get(
-        "https://api.todoist.com/rest/v1/tasks",
-        params={
-            "filter":  getFilterString()
-        },
-        headers={
-            "Authorization": getHeaderString()
-        }
-    ).json()
-
-
-def getFilterString():
-    today_date = date.today().isoformat()
-    return "(due: %s | overdue)" % today_date
-
-def getHeaderString():
-    return "Bearer %s" % secrets.API_TOKEN
-
-
-def getTasksString(today, overdue):
-
-    s = """
-☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ
-       Good morning goldsam!
-☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ☆ﾟ.*･｡ﾟ
-
-Here are today's tasks:
-"""
-    for task in today:
-        s += "□ %s\n" % task['content']
-    s += "\nHere are your overdue tasks:\n"
-    for task in overdue:
-        s += "□ %s\n" % task['content']
-    
-    s += "\n\n\n"
-
-    return s
-
-
-def getTodayAndOverdue(all_tasks):
-    today = []
-    overdue = []
-    today_date = date.today().isoformat()
-
-    for task in all_tasks:
-        due_date = task['due']['date']
-        if (due_date == today_date):
-            today.append(task)
-        else:
-            overdue.append(task)
-
-    return (today, overdue)
-
-def useSyncLibrary():
-    api = TodoistAPI(secrets.API_TOKEN)
-    api.sync()
-    #print(api.state['projects'])
-    print("\n\n\n")
-    items = api.state["items"]
-    print(json.dumps(items))
-
-    # get all completed
-    all_comp = api.completed.get_all()
-
-    for item in items:
-        if item['checked'] > 0:
-            print(item['content'])
 
 main()
 
